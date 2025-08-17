9ca9ba88de738cf628bcef4cd596ecd6afa6f0d4
Kanish Kirthik K <129273259+Kanishkirthik@users.noreply.github.com>
2025-08-17 16:09:29 +0530

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -3,9 +3,11 @@ import { Cookies } from 'react-cookie'
 import p from './profile.png'
 import { Link ,useNavigate} from 'react-router-dom'
 function Nav() {
+  //get user data form cookies based on that login/register or profile show in ui
   const cookies=new Cookies()
   let user=cookies.get('UserId')
   let navigate=useNavigate()
+  //store width of windows screen with help of useeffect  to triggers screen logo for phone
   const [width,setwidth]=useState();
   useEffect(()=>{
     let handle=function(){
@@ -48,6 +50,7 @@ function Nav() {
             </div>
         </li>
 }
+//Bases on user login/register or profile
 { !user ?<li className='li-nav'>
         <a className='login_sig' onClick={()=>{
           navigate('/login')
@@ -61,6 +64,7 @@ function Nav() {
                 <li>{cookies.get('Role')}</li>
 {cookies.get('Role')==='Recruiter' && <Link to={'/DashBoard'}><li> Go to DashBoard</li></Link>}
                 <li onClick={()=>{
+  //logout remove all data from cookies of current user.
                   cookies.remove('UserId');
                   cookies.remove('Role');
                   cookies.remove('name');
